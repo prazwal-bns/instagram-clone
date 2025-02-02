7c3d8a1874c5be802192a99995f89975e4a70b7f
prazwal-bns <prajwalbns15@gmail.com>
2025-02-02 16:41:34 +0545

    [ADD]: Added view comment of auth user

--- resources/views/livewire/post/item.blade.php
+++ resources/views/livewire/post/item.blade.php
@@ -145,6 +145,29 @@
         {{-- View Post Modal --}}
         <button onclick="Livewire.dispatch('openModal',{component: 'post.view.modal', arguments: {'post': {{ $post->id }}}})" class="text-sm font-medium text-slate-500/90">View all {{ $post->comments->count() }} comments </button>
 
+
+        @auth
+        {{-- Show auth comments  --}}
+        <ul class="my-2">   
+            @foreach ($post->comments()->where('user_id',auth()->id())->get() as $comment)
+            <li class="grid items-center grid-cols-12 text-sm">
+
+                <span class="col-span-2 mb-auto font-bold">{{auth()->user()->name}}</span>
+                <span class="col-span-9">{{$comment->body}}</span>
+                <button class="flex justify-end col-span-1 pr-px mb-auto">
+                    {{-- heart from heroicons --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.9" stroke="currentColor" class="w-3 h-3">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                      </svg>
+                </button>
+
+
+            </li>
+            @endforeach
+
+        </ul>
+        @endauth
+
         {{-- Leave comment --}}
         <form wire:key="{{ time() }}" class="grid items-center w-full grid-cols-12 " x-data="{ body:@entangle('body') }"
             @submit.prevent="$wire.addComment()">
