3c923b137fcffc2d1c77fd25b34340b7ba4b7575
prazwal-bns <prajwalbns15@gmail.com>
2025-02-06 21:59:47 +0545

    Added Explore Page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Explore;
 use App\Livewire\Home;
 use App\Livewire\Profile\Home as ProfileHome;
 use App\Livewire\Profile\Reels;
 use App\Livewire\Profile\Saved;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Home::class)->middleware('auth');
 
 Route::get('/dashboard', Home::class)->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -15,6 +15,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/', Home::class)->name('home');
+    Route::get('/explore', Explore::class)->name('explore');
 
     Route::get('/profile/{user}', ProfileHome::class)->name('profile.home');
     Route::get('/profile/{user}/reels', Reels::class)->name('profile.reels');
