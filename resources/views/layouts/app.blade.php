4837df225423b394ffdbe95705fc07f0e69ca141
prazwal-bns <prajwalbns15@gmail.com>
2025-02-16 20:42:16 +0545

    [FIX]: Fixed Sidebar issue with messages and search bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -112,7 +112,7 @@
 
             <div class="absolute inset-0 z-50 drawer-side">
                 <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
-                <livewire:components.sidebar />
+                <span><livewire:components.sidebar /></span>
             </div>
 
 
