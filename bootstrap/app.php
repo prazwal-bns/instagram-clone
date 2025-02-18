40975944077a09a2bd56667fbbf084e78dae5071
prazwal-bns <prajwalbns15@gmail.com>
2025-02-18 11:40:36 +0545

    image preview fix attempt: 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,9 +4,17 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// if (!function_exists('tmpfile')) {
+//     function tmpfile() {
+//         return fopen(tempnam(sys_get_temp_dir(), 'tmp'), 'w+');
+//     }
+// }
+
 if (!function_exists('tmpfile')) {
     function tmpfile() {
-        return fopen(tempnam(sys_get_temp_dir(), 'tmp'), 'w+');
+        $tempDir = sys_get_temp_dir();
+        $tempFile = tempnam($tempDir, 'tmp');
+        return fopen($tempFile, 'w+');
     }
 }
 
